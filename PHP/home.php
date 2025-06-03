064ecf349f189ed97bfba5ae85276ffd84fbd7e5
nightandstars <morgane.peysson@laposte.net>
2025-06-03 12:30:58 +0200

    updated home.php

--- PHP/home.php
+++ PHP/home.php
@@ -1,3 +1,6 @@
 <?php include "header.php" ?>
 
+<img src="https://t4.ftcdn.net/jpg/04/83/26/55/360_F_483265566_UZItBCmynfdu9EP32QKMadM5GcjYkyJs.jpg" class="img-fluid" alt="work in progress">
+<p class="ms-4">This is actually a placeholder since the main objective of this website is the contact page</p>
+
 <?php include "footer.php" ?>
