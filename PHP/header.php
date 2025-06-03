02ca024a6cc1f145a6d9f341a3869701aeff4f0b
MorganeP <morgane.peysson@laposte.net>
2025-06-03 11:01:29 +0200

    Create header.php

---
+++ PHP/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main header</title>
+    <meta name ="description" content="Main header for html pages defined in php">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <header class="back_color">
+        <nav id="nav_bar" class="flex">
+            <div class="dropdown">
+                <button id="btn" class="dropdown_btn">Menu</button>
+                <div id="btn_content" class="dropdown_content hidden">
+                    <a href="dnd.php">Home</a>
+                    <a href="contact.php">Contact</a>
+                </div>
+            </div>
+        </nav>
+    </header>
