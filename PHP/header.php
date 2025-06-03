7fbf36e0d12d67ff03a52903d57646a9306d5cf9
MorganeP <morgane.peysson@laposte.net>
2025-06-03 11:04:59 +0200

    Update header.php

--- PHP/header.php
+++ PHP/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main header</title>
     <meta name ="description" content="Main header for html pages defined in php">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
